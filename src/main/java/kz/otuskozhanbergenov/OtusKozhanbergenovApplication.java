f7588b5c8515fd789ad41cdc7d12978b6689b980
alibek7000 <a.kozhanbergenov@kredit24.kz>
2024-09-17 11:15:55 +0500

    Initial commit

---
+++ src/main/java/kz/otuskozhanbergenov/OtusKozhanbergenovApplication.java
@@ -0,0 +1,13 @@
+package kz.otuskozhanbergenov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusKozhanbergenovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusKozhanbergenovApplication.class, args);
+    }
+
+}
